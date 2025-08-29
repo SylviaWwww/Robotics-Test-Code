c8bc60b94f7dce528b50d4400be71d5f809151a0
FangzhengZhang <u8271898@anu.edu.au>
2025-08-29 13:27:10 +1000

    update detect_landmarks

--- lab2_robotcontrol/detect_landmarks.m
+++ lab2_robotcontrol/detect_landmarks.m
@@ -12,21 +12,22 @@ load("cameraParameters.mat")
 pb = PiBot('192.168.50.1'); % Use this command instead if using PiBot.
 
 % Create a window to visualise the robot camera
-figure;
-camAxes = axes();
 
-% First, get the current camera frame
-% img = pb.getImage();
-% imshow(img, "Parent", camAxes); % Check the video
+iteration = 1;
 while true
     img = pb.getImage();
+    % name = sprintf('landmark%d.png', iteration);
+    % imwrite(img, name);
+    num_iteration = sprintf("----------------------------iteration: %d---------------------------------", iteration);
     [marker_nums, landmark_centres, marker_corners] = detectArucoPoses(img, 0.072,cameraParameters, arucoDict);
+    disp(num_iteration);
     disp("marker nums:");
     disp(marker_nums);
     disp("landmark centres:");
     disp(landmark_centres);
     disp("marker_corners");
     disp(marker_corners);
+    iteration = iteration + 1;
     pause(2);
 end
 % attempt to detect and measure the landmark using the arucoDetector
